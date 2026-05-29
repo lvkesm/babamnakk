90f6ab3655dd26a99287744bd0523cf9f815d94d
lvkesm <simonlevente0317@gmail.com>
2026-05-29 18:43:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// --- MOBIL FIXEK ---
-// Ne zoomoljon be input fókuszkor
-document.addEventListener("touchstart", () => {
-    document.body.style.zoom = "1";
-}, { passive: true });
-
-// Küldés után visszaállítjuk a zoomot
-function resetZoom() {
-    document.body.style.zoom = "1";
-    document.documentElement.style.zoom = "1";
-}
-
-// Billentyűzet felugrásakor ne csússzon el az app
-window.addEventListener("resize", () => {
-    document.querySelector(".app").style.height = window.innerHeight + "px";
-});
-
-
-// --- EREDETI KÓD + MOBIL FIXEK ---
 const names = [
   "Kicsim💖",
   "Szerelmem💗",
@@ -162,12 +143,6 @@ function sendMessage(){
 
   input.value = "";
 
-  // --- ZOOM RESET KÜLDÉS UTÁN ---
-  setTimeout(() => {
-    input.blur();
-    resetZoom();
-  }, 150);
-
   if(text.includes("tipp") || text.includes("igen")){
 
     thinking(() => {
@@ -220,16 +195,19 @@ function sendMessage(){
 
       } else {
 
+        // 🔥 AUTOMATIKUS VÉGÜZENET + EMAIL KÜLDÉS
         setTimeout(() => {
 
           createMessage(
             "Megtaláltad az összes ajándékot! 💖\nNézd meg az e-mailjeidet 😉"
           );
 
+
+          // EmailJS automatikus futtatás
           emailjs.send("service_8uk3dwk","template_hj7viip",{
-          name: "asd",
-          message: "asd",
-          email: "sion7sion69@gmail.com",
+          name: "Férjed",
+          message: "Boldog 2 hónapot életemm! 💗 https://www.youtube.com/watch?v=LDqecs-AFc8",
+          email: "leilakaldi07.com",
           });
 
         }, 1200);
